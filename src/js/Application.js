e8c5efbd6ed1ef36d5eb91cd718bd0c6d767a20d
Vanya Dimitrova <vani4ka66@abv.bg>
2021-09-18 21:55:25 +0300

    Commit 1

--- src/js/Application.js
+++ src/js/Application.js
@@ -13,6 +13,10 @@ export default class Application extends EventEmitter {
 
     const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
     let count = 0;
+    this._beat = new Beat();
+    this.ll = lyrics;
+
+    this._create()
 
     const message = document.createElement("div");
     message.classList.add("message");
@@ -21,5 +25,11 @@ export default class Application extends EventEmitter {
     document.querySelector(".main").appendChild(message);
 
     this.emit(Application.events.READY);
+  }
+
+  _create(){
+
+    let b = new Beat();
+
   }
 }
--- src/js/Beat.js
+++ src/js/Beat.js
@@ -1,4 +1,7 @@
-export default class Beat {
+import EventEmitter from "eventemitter3";
+import Application from "./Application";
+
+export default class Beat extends EventEmitter {
   static get events() {
     return {
       BIT: "bit",
@@ -6,8 +9,29 @@ export default class Beat {
   }
 
   constructor() {
+    super()
+
+    const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
+
     setInterval(() => {
       console.log("bit");
+
+      for (let i = 0; i < lyrics.length; i++) {
+        const element = lyrics[i];
+
+        let a = document.createElement('div');
+        a.classList.add('message');
+        let main = document.getElementsByClassName('main')[0];
+        main.appendChild(a)
+        this.emit(Beat.events.BIT);
+
+        console.log(lyrics[i])
+        a.innerHTML = lyrics[i]
+        
+      }
+
     }, 600);
+
+
   }
 }
