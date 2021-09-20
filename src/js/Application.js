8fac3f392773c36936b38271790d98f099486006
Vanya Dimitrova <vani4ka66@abv.bg>
2021-09-20 19:49:09 +0300

    Commit 18

--- src/js/Application.js
+++ src/js/Application.js
@@ -14,7 +14,7 @@ export default class Application extends EventEmitter {
     const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
     let count = 0;
     
-    this._beat = new Beat().addListener('listen', () => {
+    this._beat = new Beat().on('listen', () => {
 
       if (count == 6) {
         count = 0;
